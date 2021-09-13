edf3ef56053993e20b322dd7e7f1e76e5e2f354a
sagar <sagarkumarmohapatra5@gmail.com>
2021-09-13 12:12:28 +0530

    React basic hook

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Card from './components/Card'
+
+
+let App = () => {
+  return (
+    <React.Fragment>
+      <nav className="navbar navbar-expand-lg navbar-dark primary-color">
+        <a href="" className="navbar-brand">React Hooks Basics</a>
+      </nav>
+      <Card />
+    </React.Fragment>
+  )
+}
+
+
+
+
+
+
+export default App;
+
---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Cardcss.css'
+
+
+let Card = () => {
+    return (
+        <React.Fragment>
+            <div className="card">
+                <div className="card-body">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, dolorum! Assumenda sapiente qui molestiae autem, aperiam iste. Officiis consequuntur velit possimus quis! Cumque eveniet repellendus nisi placeat deserunt minima non.</p>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
+
+
+export default Card;
+
